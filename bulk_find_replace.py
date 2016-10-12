0706202659afff2a844b44ce48b5c623538c4b52
Jacob Sanford <jsanford@unb.ca>
2016-10-12 09:28:31 -0300

    Convert to composer.json parsing, updates

--- bulk_find_replace.py
+++ bulk_find_replace.py
@@ -6,6 +6,7 @@
 from github import Github
 from git import Repo
 from optparse import OptionParser
+import base64
 import json
 import shutil
 import tempfile
@@ -14,7 +15,7 @@ import os
 import re
 
 include_repo_match = ''
-file_to_modify = os.path.join('build', 'make.yml')
+file_to_modify = os.path.join('build', 'composer.json')
 pause_seconds = 30
 
 parser = OptionParser()
@@ -32,53 +33,53 @@ config = json.load(open(args[0]))
 g = Github(config['github_auth_key'])
 org_repos = g.get_organization('unb-libraries').get_repos()
 num_repos = len(list(org_repos))
-cur_repo_count = 0
+
 for repo in org_repos:
     if include_repo_match == '' or include_repo_match in repo.name:
-        cur_repo_count += 1
-        print '[' + str(cur_repo_count) + '/' + str(num_repos) + '] Checking ' + repo.name + "\n"
         try:
-            repo.get_file_contents(file_to_modify)
+            repo_needs_update = False
+            file_contents = repo.get_file_contents(file_to_modify)
 
-            if options.print_only:
-                print '[DEBUG] Searching inside ' + repo.name
-
-            tmp_dirpath = tempfile.mkdtemp()
-            cur_repo = Repo.clone_from(repo.ssh_url, tmp_dirpath)
-
-            makefile_has_commits = False
+            assert file_contents.encoding == "base64", "unsupported encoding: %s" % file_contents.encoding
+            composer_contents = base64.b64decode(file_contents.content)
+            composer_data = json.loads(composer_contents)
 
             for update in config['updates']:
-                find_string = update['project'] + ":\n    version: '" + update['old']
-                replace_string = update['project'] + ":\n    version: '" + update['new']
-                commit_message = update['project'] + ' ' + update['old'] + ' -> ' + update['new']
-                if 'comments' in update:
-                    commit_message += ' ' + update['comments']
-                file_to_edit = os.path.join(tmp_dirpath, file_to_modify)
+                project_identifier = 'drupal/' + update['project']
+                if composer_data['require'][project_identifier] == update['old']:
+                    # There is at least one update to this repo.
+                    repo_needs_update = True
+                    break
 
-                with open(file_to_edit, 'r+b') as editfile_pointer:
-                    editfile_content = editfile_pointer.read()
-                    if find_string in editfile_content:
-                        editfile_content_updated = editfile_content.replace(find_string, replace_string)
-                        editfile_pointer.seek(0)
-                        editfile_pointer.write(editfile_content_updated)
-                        editfile_pointer.truncate()
-                        editfile_pointer.close()
-                    if cur_repo.is_dirty():
-                        print cur_repo.git.diff()
+            if repo_needs_update:
+                tmp_dirpath = tempfile.mkdtemp()
+                cur_repo = Repo.clone_from(repo.ssh_url, tmp_dirpath)
+
+                file_to_edit = os.path.join(tmp_dirpath, file_to_modify)
+                with open(file_to_edit, 'r') as f:
+                    composer_data = json.load(f)
+
+                for update in config['updates']:
+                    project_identifier = 'drupal/' + update['project']
+                    if composer_data['require'][project_identifier] == update['old']:
+                        composer_data['require'][project_identifier] = update['new']
+                        with open(file_to_edit, 'w') as f:
+                            json.dump(composer_data, f, indent=4)
+
+                        commit_message = update['project'] + ' ' + update['old'] + ' -> ' + update['new']
+                        if 'comments' in update:
+                            commit_message += ' ' + update['comments']
+
                         print cur_repo.git.add(file_to_modify)
                         print cur_repo.git.commit(m=commit_message)
-                        makefile_has_commits = True
-                        print cur_repo.git.status()
 
-                if makefile_has_commits:
-                    if options.print_only:
-                        print "[DEBUG] Dry-run only. Not pushing to GitHub."
-                    else:
-                        print cur_repo.remotes.origin.push(cur_repo.head)
+                if options.print_only:
+                    print "[DEBUG] Dry-run only. Not pushing to GitHub."
+                else:
+                    print cur_repo.remotes.origin.push(cur_repo.head)
 
-                    print "Sleeping for " + str(pause_seconds) + " seconds to be polite.."
-                    time.sleep(pause_seconds)
+                print "Sleeping for " + str(pause_seconds) + " seconds to be polite.."
+                time.sleep(pause_seconds)
 
             shutil.rmtree(tmp_dirpath)
 
