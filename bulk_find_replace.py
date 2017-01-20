4d47076d74f31166256be976b57541a2738c3a47
Jacob Sanford <jsanford@unb.ca>
2017-01-20 10:00:08 -0400

    Add newline to EOF

--- bulk_find_replace.py
+++ bulk_find_replace.py
@@ -64,7 +64,7 @@ for repo in org_repos:
                     if project_identifier in composer_data['require'] and composer_data['require'][project_identifier] == update['old']:
                         composer_data['require'][project_identifier] = update['new']
                         with open(file_to_edit, 'w') as f:
-                            json.dump(composer_data, f, indent=4, sort_keys=True)
+                            json.dump(composer_data, f, indent=4, sort_keys=True) + "\n"
 
                         commit_message = update['project'] + ' ' + update['old'] + ' -> ' + update['new']
                         if 'comments' in update:
